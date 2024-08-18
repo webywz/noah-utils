3e9261607cb94753bce55102d3c35a3adeac3b99
ywz <37465255+webywz@users.noreply.github.com>
2024-08-18 20:14:15 +0800

    更新版本

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module 'formatCurrency' {
+declare module 'library/index' {
     // 声明内容
     function formatCurrency(amount: number | string, decimalPlaces: number, showDecimals:boolean, zeroAsEmpty: boolean): string;
 }
